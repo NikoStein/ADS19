1fe9f3d71dd1381cdff8b0bb5c73672066c934e0
CF <christoph.flath@uni-wuerzburg.de>
2019-07-16 12:46:20 +0200

    data added

--- Lecture Code/plumber.R
+++ Lecture Code/plumber.R
@@ -18,7 +18,8 @@ function(){
 #* Return the sum of two numbers
 #* @param a The first number to add
 #* @param b The second number to add
-#* @post /sum
+#* @get /sum
 function(a, b){
   as.numeric(a) + as.numeric(b)
 }
+
