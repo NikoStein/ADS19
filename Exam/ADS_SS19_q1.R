3fdeede913e766b8dd83a86a5aaac7fd3aac4c43
Nikolai Stein <nikolai.stein86@gmail.com>
2019-07-22 13:19:23 +0200

    added templates

---
+++ Exam/ADS_SS19_q1.R
@@ -0,0 +1,33 @@
+# Matriculation number: YOUR_MATRICULATION_NUMBER
+
+# Question 1:
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(randomNames, tidyverse)
+
+firstName = randomNames(100, which.names = "first", sample.with.replacement = FALSE)
+lastName = randomNames(100, which.names = "last", sample.with.replacement = FALSE)
+
+names = data.frame(firstName, lastName)
+
+# a)
+createEmail = function(firstName, lastName){
+  # Write your code here
+}
+
+
+# b)
+createPassword = function(){
+  # Write your code here
+}
+
+
+# c)
+createUserName = function(firstName, lastName){
+  # Write your code here
+}
+
+
+# d)
+completeDataset = function(firstName, lastName){
+  # Write your code here
+}
---
+++ Exam/ADS_SS19_q2.R
@@ -0,0 +1,16 @@
+# Matriculation number: YOUR_MATRICULATION_NUMBER
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse)
+
+economics = ggplot2::economics
+
+
+# a)
+# Write your code here
+
+# b)
+# Write your code here
+
+# c)
+# Write your code here
